b56bf1bd890815845b2ad02bf5f3df55ba9526ae
Eric Beragg <eric.beragg@gmail.com>
2018-04-10 14:24:07 +0200

    Added TSLint dependency
    I have no clue yet which configuration I want to use, or wether I
    really want TSLint vs ESLint with an adaptor. Anyways, this gives me
    some linting functionality which is always good.

--- src/core/fileSystem.ts
+++ src/core/fileSystem.ts
@@ -1,62 +1,62 @@
 import * as fs from 'fs';
-import * as Rx from 'rxjs';
 import * as Path from 'path';
+import * as Rx from 'rxjs';
 
-// 
+//
 // Recursively asynchronously enumerates the descendent
 // files and directories of the given root directory.
-// 
+//
 export const traverse$ = (dir: string) => {
   return ls$(dir)
-    .expand(x => {
+    .expand((x) => {
       return x.stat.isFile() ?
         Rx.Observable.empty() :
-        ls$(x.path)
-    })
+        ls$(x.path);
+    });
 };
 
-// 
+//
 // Asynchronously list the file stats of a directory.
-// 
+//
 export const ls$ = (dir: string) => {
-  return readdir$(dir).flatMap(files => {
+  return readdir$(dir).flatMap((files) => {
     return Rx.Observable.from(files.map(stat$)).mergeAll();
   });
 };
 
-// 
+//
 // Asynchronously reads the files in the directory and emits an Array of dir + filename.
-// 
+//
 export const readdir$ = (dir: string) => {
   return Rx.Observable.bindNodeCallback(fs.readdir, (files: string[]) => {
-    return files.map(file => Path.join(dir, file));
+    return files.map((file) => Path.join(dir, file));
   })(dir);
 };
 
-// 
+//
 // Asynchronously reads the stats of the item at the path.
-// 
+//
 export const stat$ = (path: string) => {
-  return Rx.Observable.bindNodeCallback(fs.stat, stat => {
+  return Rx.Observable.bindNodeCallback(fs.stat, (stat) => {
     return getFilenameMetaData(path || '', { stat });
   })(path);
 };
 
-interface FilenameMetadata {
-  extension: string,
-  name: string,
-  location: string,
-  path: string,
-  stat: fs.Stats,
-};
+interface IFilenameMetadata {
+  extension: string;
+  name: string;
+  location: string;
+  path: string;
+  stat: fs.Stats;
+}
 
-const getFilenameMetaData = (path, additionalMetadata): FilenameMetadata => {
-  var extension = Path.extname(path);
+const getFilenameMetaData = (path, additionalMetadata): IFilenameMetadata => {
+  const extension = Path.extname(path);
   return {
     extension: Path.extname(path),
-    name: Path.basename(path, extension),
     location: Path.dirname(path),
-    path: path,
+    name: Path.basename(path, extension),
+    path,
     ...additionalMetadata,
   };
 };
